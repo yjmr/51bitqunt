c155d4bcee11711c0d02f00983fc007721528a33
yjmr <1424194274@qq.com>
2019-11-15 21:06:29 +0800

    from yj 将配置文件路径修改为相对智能的路径

--- test/test_savg.py
+++ test/test_savg.py
@@ -3,30 +3,18 @@ import configparser
 import pymysql
 import os
 import sys
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
-sys.path.append(BASE_DIR)
+up_one_level = os.path.dirname(__file__)
+base_dir_path = os.path.dirname(up_one_level)
+sys.path.append(base_dir_path)
 from  DBConnect import DBConnect
 
-print('test')
-
-
-
-
+keys = sys.argv[1]
+print(keys)
 conf = configparser.ConfigParser()
-conf_path = r'F:\code\github_files\51bitqunt\test\config\db_config.config'
+
+conf_path = base_dir_path + r'/test/config/db_config.config'
 conf.read(conf_path, encoding ='utf-8')
 
-
-# keys = sys.argv[1]
-# print(keys)
-# print(conf.get(keys,'host'))
-# print(conf.get(keys,'port'))
-# print(conf.get(keys,'user'))
-# print(conf.get(keys,'password'))
-# print(conf.get(keys,'db'))
-
-keys = 'jd'
 host_value = conf.get(keys,'host')
 port_value = conf.get(keys,'port')
 user_value = conf.get(keys,'user')
@@ -36,10 +24,5 @@ db_value = conf.get(keys,'db')
 DBConnect.get_connect(host_value,port_value,user_value,password_value,db_value)
 
 
-conn = pymysql.connect(host=host_value, port=int(port_value),user=user_value, password=password_value, database=db_value,charset='utf8')
-cursor  = conn.cursor()   #初始化游标
-sql = 'show databases'
-cursor.execute(sql)
-text = cursor.fetchall()
-print(text)
+
 
