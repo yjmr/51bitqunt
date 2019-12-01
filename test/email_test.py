ef9b613fe86f1899a964a7a827f07cb457f9f6de
yjmr <1424194274@qq.com>
2019-12-01 21:45:29 +0800

    from yj 优化代码

--- test/email_test.py
+++ test/email_test.py
@@ -5,7 +5,7 @@ contents = ['This is the body, and here is just text http://somedomain/image.png
 def get_email(send_text,subject_text):
     # 发邮件
     # 链接邮箱服务器
-    # yag = yagmail.SMTP(user="xxx@.com", password="授权码", host='smtp.qq.com')
+    yag = yagmail.SMTP(user="xxx@.com", password="授权码", host='smtp.qq.com')
     subject = subject_text + '核对不上,请核查'
     # 发送邮件
     yag.send('yj624@foxmail.com', subject, send_text)
