bc6dd227bf7e63c49a9178a2ee4954c002962547
eddie <eddie>
2019-12-19 15:59:38 +0800

    from yj

---
+++ test/socket_test/test_socket.py
@@ -0,0 +1,8 @@
+import logging
+logging.debug('debug message') #低级别的：排错信息
+logging.info('debug message') #正常信息
+logging.warning('warning message')#警告信息
+logging.error('error message') #错误信息
+logging.critical('critical message') #高级别的  ： 严重错误信息
+
+
