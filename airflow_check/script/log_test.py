36a23178a383a948a69e38d57ece78b0f4ecd76d
yjmr <1424194274@qq.com>
2020-03-17 22:30:59 +0800

    from yj

--- airflow_check/script/log_test.py
+++
@@ -1,5 +0,0 @@
-from loguru import logger
-logger.add("file.log", format="{time} {level} {message}", filter="", level="INFO",encoding='utf-8')
-logger.debug("这是一条debug日志")
-logger.info("这是一条info日志")
-logger.info("这是一条info2日志")
